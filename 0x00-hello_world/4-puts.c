19e956b505c68748dbf2e4a10759a466c75c9626
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-16 10:57:41 -0800

    updating on file 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main -  prints "programming is like building
  * a multilingual puzzle,followed by a new line"
@@ -12,5 +13,5 @@ int main(void)
 	
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+	return(0);
 }
