11602e48ab0ed61ed4a1d6b98ad7029e215cf196
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-16 11:41:03 -0800

    task number five

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print "with proper grammer ,but outcome is 
+ * a piece of art," followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	printf("with proper grammer ,but outcome is a piece of art,\n");
+	return(0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,3 +1,4 @@
 a script that runs a C file through the preprocessor and save the result into another file.
  a script that compiles a C file but does not link
 assembler file
+printf with proper grammer file
