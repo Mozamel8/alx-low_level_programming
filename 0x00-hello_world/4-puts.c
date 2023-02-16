3599ae61cd826d5935f61a043330d10bb50b81c1
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-16 11:11:46 -0800

    new update in file 4-

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 
 /**
- * main -  prints "programming is like building
- * a multilingual puzzle,followed by a new line"
+ * main -  prints "Programming is like building
+ *  a multilingual puzzle"
  * return 0
  */
 
