57e6be7c82343c32e4713c3e983616ca8f878eb1
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-17 07:19:56 -0800

    task number2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	purchar("\n");
+	return (0);
+}
