aa03044c09657653d6e8821d2a84e3fdd192303a
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-17 12:52:21 -0800

    tasknumber five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all digit number base ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for(int i = 0; i<= 10; i++)
+	printf("%d", i);
+
+	print ('\n');
+
+	return (0);
+}
