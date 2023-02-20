56c23b3fbcff7cc741988877027a83eadba7ee2d
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-19 20:35:03 -0800

    mmmm

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print cobination of two two-digit number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i+1; j < 58; 
+	}
