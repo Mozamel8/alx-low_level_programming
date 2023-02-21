de2f87a4b44019e6b1dd2c0bc4d072ddb7db5084
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-21 01:41:26 -0800

    update on 0-put file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,21 +2,21 @@
 
 /**
  * main - Entry point
- * print "_putchar"
+ * print _putchar
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	_purchar("_");
-	_purchar("p");
-	_purchar("u");
-	_purchar("t");
-	_purchar("c");
-	_purchar("h");
-	_purchar("a");
-	_purchar("r");
-	_purchar("\n");
+	_purchar('_');
+	_purchar('p');
+	_purchar('u');
+	_purchar('t');
+	_purchar('c');
+	_purchar('h');
+	_purchar('a');
+	_purchar('r');
+	_purchar('\n');
 	return (0);
 }
