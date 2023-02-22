1969ba3dfdf7907f4a0e1bb8785538b2eb745bcc
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-21 18:01:34 -0800

    add task number 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *
+ * @c: Number to process
+ *
+ * Return: last digit
+ */
+
+int print_last_digit(int c)
+{
+	int lastDig;
+
+	lastDig = c % 10;
+
+	return (lastDig);
+}
