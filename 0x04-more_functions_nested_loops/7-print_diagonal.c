16c1af8ca38bb4990b06dfaa1ff30ea10b13e27a
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-23 11:22:19 -0800

    task number 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ *
+ * @n: number to print line
+ * Return: void
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 1; i <= n; i++)
+	{
+		for(j = 1; j <= i; j++)
+		{
+		_putchar(32);
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
