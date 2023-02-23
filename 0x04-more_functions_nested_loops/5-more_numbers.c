6a870f8245c07c5e0c5e77827954b96ddef34b41
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-23 10:24:59 -0800

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print from 0 to 14
+ * 10 times
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+			_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
