ae8db029b603c48efdcbd51c0238b53588c060da
Mozamel8 <mozamelmustafa87@gmail.com>
2023-02-27 23:10:23 -0800

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - print string
+ * @str: variable of string
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
