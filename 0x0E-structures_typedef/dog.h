221cb915752df7826a9affe585d6b781d8a4e4f1
Mozamel8 <mozamelmustafa87@gmail.com>
2023-03-20 12:20:33 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * struct dog - dog attribute
+ * @name:name of dog
+ * @age:age of dog
+ * @owner:owner of dog
+*/
+
+struct dog
+{
+	char * name;
+	float age;
+	char *owner;
+};
+
+#endif
