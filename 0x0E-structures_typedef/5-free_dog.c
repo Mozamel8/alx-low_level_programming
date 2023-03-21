a03f5b1186a42a2eb0df76ceda90bc355b8b8911
Mozamel8 <mozamelmustafa87@gmail.com>
2023-03-21 01:04:19 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - functon to free structur
+ *
+ * @d:structure to be free
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d !=NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
