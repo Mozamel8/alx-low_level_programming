ebb0c0118713856d11b9e3097dab71a4bf328874
Mozamel8 <mozamelmustafa87@gmail.com>
2023-03-20 20:46:27 -0700

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,19 +9,12 @@
  * Description:the attribute dog
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for dog structur
-*/
-
-typedef struct dog dog_t;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
