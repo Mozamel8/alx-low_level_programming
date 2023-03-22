9e086068412416dd4914747ddfa06ae3c7e09ae4
Mozamel8 <mozamelmustafa87@gmail.com>
2023-03-22 13:55:00 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * op_add - add function
+ * @a:number one
+ * @b:number 2
+ * Return: integer
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub function
+ * @a:number one
+ * @b:number two
+ * Return: integer
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multipy function
+ * @a:number one
+ * @b:number 2
+ * Return:integer
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function divide number
+ * @a:number one
+ * @b:number two
+ * Return:integer
+*/
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - funtion return modular
+ * @a:number 1
+ * @b:number 2
+ * Return:integer
+*/
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
+
+
