1e9ff7235b189981f52b2f5b4a05b900998c1123
Mozamel8 <mozamelmustafa87@gmail.com>
2023-03-30 05:48:08 -0700

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include  <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * add_node - add new node at the beginning
+ * @head:list_t struct point to head
+ * @str:string to be print
+ * Return:new node
+*/
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_nod = malloc(sizeof(list_t));
+
+	new_nod->str = strdup(str);
+	new_nod->len = strlen(str);
+	new_nod->next = *head;
+	*head = new_nod;
+
+	if (!new_nod)
+		return (NULL);
+	else
+		return (new_nod);
+}
