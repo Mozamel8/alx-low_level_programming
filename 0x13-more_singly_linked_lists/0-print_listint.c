8c20c485f8d830117e29fbcd0317376355e5c62b
Mozamel8 <mozamelmustafa87@gmail.com>
2023-04-03 08:10:37 -0700

    updata task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,8 +12,6 @@ size_t print_listint(const listint_t *h)
 
 	size_t counter = 0;
 
-	
-
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint- function to free linked
+ * *head:head of linkedlist
+ * Return:free linkedlist
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp = malloc(sizeof(listint_t));
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
