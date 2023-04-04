c3374a07ba43baf0ece5b8c1854b43c6b50c9eff
Mozamel8 <mozamelmustafa87@gmail.com>
2023-04-04 13:31:27 -0700

    update task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,13 +5,12 @@
 /**
  * pop_listint - function to delete
  * @head:head of linkedlist
- * @n:data of node
  * Return:nod's data
 */
 
 int pop_listint(listint_t **head)
 {
-	listint_t *ptr = *head;
+	listint_t *ptr = (*head);
 
 	if ((*head) == NULL)
 	return (0);
