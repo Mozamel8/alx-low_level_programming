0472ecc739c76b1a2f210cc87232b20ce4697c9f
Mozamel8 <mozamelmustafa87@gmail.com>
2023-04-04 14:24:13 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,6 +5,7 @@
 
 /**
  * get_nodeint_at_index - function return nth node
+ * @head:head of node
  * @index:to returned node
  * Return:node
 */
@@ -13,7 +14,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp = head;
 	unsigned int c = 0;
-	
+
 	if (!temp)
 		return (NULL);
 	while (temp != NULL)
