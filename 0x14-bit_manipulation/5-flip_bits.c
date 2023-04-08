f047f3e8f44d2946831a35488c4d2e2ae70bc1a2
Mozamel8 <mozamelmustafa87@gmail.com>
2023-04-08 01:41:39 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * flip_bits - function to flip number to another
+ * @n:number 1
+ * @m:number 2
+ * Return:number of converted
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+}
